b541f8d7abe92d0e8bf328ebb712b707f368e21a
JulNiki <166639724+JulNiki@users.noreply.github.com>
2024-04-21 03:31:25 +0300

    Update Example1.java

--- src/Example1.java
+++ src/Example1.java
@@ -1,5 +1,4 @@
-/*Создайте метод printThreeWords(), который при вызове
-должен отпечатать в столбец три слова: Orange, Banana, Apple  */
+/*Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple  */
 
 class Example1 {
     public static void main(String[] args) {

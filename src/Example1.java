67d731499d7db6470793ff477d700ba47f6006d6
Юлия <9925139@gmail.com>
2024-04-21 03:19:28 +0300

    /

---
+++ src/Example1.java
@@ -0,0 +1,10 @@
+class Example1 {
+    public static void main(String[] args) {
+        printThreeWords ();
+    }
+        static void printThreeWords () {
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+        }
+}

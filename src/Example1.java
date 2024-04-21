70c555e49c52ff6bb060048e3859a9f61d263390
Юлия <9925139@gmail.com>
2024-04-21 03:28:54 +0300

    задание 1

--- src/Example1.java
+++ src/Example1.java
@@ -1,3 +1,6 @@
+/*Создайте метод printThreeWords(), который при вызове
+должен отпечатать в столбец три слова: Orange, Banana, Apple  */
+
 class Example1 {
     public static void main(String[] args) {
         printThreeWords ();

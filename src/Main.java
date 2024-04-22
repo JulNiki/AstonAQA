8ac23931f80b5209e8319dd6e6af50ffb2939cca
Юлия <9925139@gmail.com>
2024-04-22 10:16:14 +0300

    исправление

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,104 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Main {
+    public static void main(String[] args) {
+        printThreeWords (); //task 1
+        checkSumSign ( 6, -7); //task 2
+        printColor (101); //task 3
+        compareNumbers (2, 1); //task 4
+        System.out.println(sumBtwn10and20 (3, 7)); //task 5
+        positiveOrNegative(-1); //task 6
+        System.out.println(numNegative(0)); //task 7
+        printStringHowManyTimes("ASTON", 4); //task 8
+        System.out.println(isLeapYear(2000));
+
+
+    }
+    //task 1
+    static void printThreeWords () {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    //task 2
+    static void checkSumSign (int a, int b) {
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
         }
     }
+    //task 3
+    static void printColor (int value) {
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if ( value <= 100 ) {
+            System.out.println("Желтый");
+        } else  {
+            System.out.println("Зеленый");
+        }
+    }
+
+    //task 4
+    static void compareNumbers (int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else  {
+            System.out.println("a < b");
+        }
+    }
+
+    //task 5
+    static boolean sumBtwn10and20 (int a, int b) {
+        boolean result;
+        result = ((a + b) >= 10) && ((a + b) <= 20);
+        return result;
+    }
+
+    //task 6
+    static void positiveOrNegative (int a) {
+        if (a >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+
+    //task 7
+    static boolean numNegative (int a) {
+        boolean result;
+        result = (a < 0);
+        return result;
+    }
+
+    //task 8
+    static void printStringHowManyTimes(String str, int times) {
+        int x = 0;
+        while (x < times) {
+            System.out.println(str);
+            x++;
+        }
+    }
+
+    //task 9
+    static boolean isLeapYear(int year) {
+        boolean result;
+        result = ((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0 && year % 400 == 0));
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

66c63a91ca75c1a367869e842d4f97c3497b296b
Юлия <9925139@gmail.com>
2024-04-22 12:35:00 +0300

    исправление

--- src/MySolution.java
+++ src/MySolution.java
@@ -1,5 +1,5 @@
 
-class Main {
+class MySolution {
     public static void main(String[] args) {
         printThreeWords (); //task 1
         checkSumSign ( 6, -7); //task 2
@@ -9,7 +9,16 @@ class Main {
         positiveOrNegative(-1); //task 6
         System.out.println(numNegative(0)); //task 7
         printStringHowManyTimes("ASTON", 4); //task 8
-        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(2000));//task 9
+        int[] myArr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        replace0And1InArray(myArr);//task 10
+        createArrayLength100from1to100();//task 11
+        int[] Arr12 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        multiplyBy2NumLess6InArray(Arr12);//task 12
+        int[][] myArr13 = new int[5][5];
+        fillDiagonalElmntsInAarrayOnes(myArr13);//task 13
+        int[] myArr14 = makeArrayLenValue(4, 3);//task 14
+        for (int j : myArr14) System.out.print(j + " ");
 
 
     }
@@ -84,6 +93,65 @@ class Main {
         boolean result;
         result = ((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0 && year % 400 == 0));
         return result;
+    }
+
+    //task 10
+    static void replace0And1InArray(int[] Arr) {
+        for (int i = 0; i < Arr.length; i++) {
+            if (Arr[i] == 0) {
+                Arr[i] = 1;
+            } else if (Arr[i] == 1) {
+                Arr[i] = 0;
+            }
+        }
+        for (int j : Arr) System.out.print(j + " ");
+    }
+
+    //task 11
+    static void createArrayLength100from1to100() {
+        int[] myArr = new int[100];
+        for (int i = 0; i < myArr.length; i++) {
+            myArr[i] = i + 1;
+        }
+        for (int j : myArr) System.out.print(j + " ");
+    }
+
+    // task12
+    static void multiplyBy2NumLess6InArray(int[] Arr) {
+        for (int i = 0; i < Arr.length; i++) {
+            if (Arr[i] < 6) {
+                Arr[i] = Arr[i] * 2;
+            }
+        }
+        for (int j : Arr) System.out.print(j + " ");
+    }
+
+    //task 13
+    static void fillDiagonalElmntsInAarrayOnes (int[][] Arr){
+        for (int i = 0; i < Arr.length; i++) { //myArr.length - количество строк
+            for (int j = 0; j < Arr[i].length; j++) { //myArr[i].length - размер i-й строки
+                if (i == j) {
+                    Arr[i][j] = 1;
+                }
+                if ((i + j) == (Arr[i].length - 1)) {
+                    Arr[i][j] = 1;
+                }
+
+                System.out.print(Arr[i][j] + "\t");
+            }
+            System.out.println();
+
+        }
+    }
+
+    //task 14
+    static int[] makeArrayLenValue(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = initialValue;
+        }
+        return arr;
+
     }
 
 

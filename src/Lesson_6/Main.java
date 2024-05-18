1b9c7693453dda5a735dd1a0beddbbb78976df54
Юлия <9925139@gmail.com>
2024-05-18 12:17:01 +0300

    6

--- src/Lesson_6/Main.java
+++ src/Lesson_6/Main.java
@@ -2,7 +2,7 @@ package Lesson_6;
 public class Main {
     public static void main(String[] args) {
         Employee[] person = new Employee[5];
-        person[0] = new Employee("Яхин Филипп Игнатьевич", "Директор", "filipp2595@hotmail.com", "+7 (952) 653-19-96", 74000, 63);
+        person[0] = new Employee("Cи Филипп Игнатьевич", "Директор", "filipp2595@hotmail.com", "+7 (952) 653-19-96", 74000, 63);
         person[1] = new Employee("Тевосов Максим Павлович", "Сварщик", "maksim67@yandex.ru", "+7 (909) 419-56-56", 52000, 34);
         person[2] = new Employee("Еромеева Рада Валентиновна", "Диспетчер", "rada2022@outlook.com", "+7 (949) 831-46-56", 34000, 25);
         person[3] = new Employee("Румянцева Валентина Порфнрьевна", "Специалист по ВЭД", "valya7387@gmail.com", "++7 (905) 286-93-14", 65000, 46);
